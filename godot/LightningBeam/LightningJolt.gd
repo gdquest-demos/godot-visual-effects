b4627d076b4b63ec31d0c7f252a6092e9ef8e6d2
Nathan Lovato <nathan@gdquest.com>
2020-07-15 14:19:05 -0600

    Format code with gdformat

--- godot/LightningBeam/LightningJolt.gd
+++ godot/LightningBeam/LightningJolt.gd
@@ -14,25 +14,25 @@ func _ready() -> void:
 
 func create(start: Vector2, end: Vector2) -> void:
 	point_end = end
-	
+
 	var _points := []
 	var _start := start
 	var _end := point_end
-	var _segment_length := _start.distance_to(_end)/segments
-	
+	var _segment_length := _start.distance_to(_end) / segments
+
 	_points.append(_start)
-	
+
 	var _current := _start
-	
+
 	for segment in range(segments):
 		# Face the end point and extend towards it
 		# Rotate a random amount to get next point
-		var _rotation := rand_range(-spread_angle/2, spread_angle/2)
+		var _rotation := rand_range(-spread_angle / 2, spread_angle / 2)
 		var _new := _current + (_current.direction_to(_end) * _segment_length).rotated(_rotation)
 		_points.append(_new)
 		_current = _new
-		
+
 	_points.append(_end)
 	points = _points
-	
+
 	sparks.global_position = point_end
