16a79013e4f1df6560743a2375c23cd0b298622a
johnnygossdev <me@johnnygoss.dev>
2020-07-16 12:22:45 +0100

    Update lightning effect to cast onto the surface of asteroids

--- godot/LightningBeam/LightningBeam.gd
+++ godot/LightningBeam/LightningBeam.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends RayCast2D
 
 export var lightning_container_path: NodePath
 export var flashes := 3
@@ -7,8 +7,17 @@ export var lightning_jolt: PackedScene = preload("res://LightningBeam/LightningJ
 onready var lightning_container: Node = self
 
 
+func _physics_process(delta):
+	cast_to = get_local_mouse_position()
+
+
 func shoot() -> void:
 	var target_point := get_global_mouse_position()
+
+	force_raycast_update()
+
+	if is_colliding():
+		target_point = get_collision_point()
 
 	for flash in range(flashes):
 		var _new_jolt = lightning_jolt.instance()
