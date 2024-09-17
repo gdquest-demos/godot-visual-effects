5acb6309bc0dd8ac066666967da69bb69e7d8691
HubbleCommand <39958198+HubbleCommand@users.noreply.github.com>
2024-09-17 11:37:18 +0200

    partial update GhostTrail

--- godot/GhostTrail/FadingSprite.gd
+++ godot/GhostTrail/FadingSprite.gd
@@ -1,8 +1,6 @@
-extends Sprite
+extends Sprite2D
 
-export var lifetime := 0.5
-
-onready var _tween := $Tween
+@export var lifetime := 0.5
 
 
 func _ready() -> void:
@@ -12,7 +10,7 @@ func _ready() -> void:
 func fade(duration := lifetime) -> void:
 	var transparent := self_modulate
 	transparent.a = 0.0
-	_tween.interpolate_property(self, "self_modulate", self_modulate, transparent, duration)
-	_tween.start()
-	yield(_tween, "tween_all_completed")
+	var tween = create_tween()
+	tween.tween_property(self, "self_modulate", transparent, duration).from_current()
+	await tween.finished
 	queue_free()
